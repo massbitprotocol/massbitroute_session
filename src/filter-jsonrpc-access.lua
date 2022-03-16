8307c6ca9aa1fdbd268cedb14166e6073ba1165d
Vu Tran <baysao@gmail.com>
2022-03-16 14:46:30 +0700

    change http code 307 -> 308

--- src/filter-jsonrpc-access.lua
+++ src/filter-jsonrpc-access.lua
@@ -1,10 +1,10 @@
 local set_var = ndk.set_var
 
-local cjson = require("cjson")
+-- local cjson = require("cjson")
 
-local function empty(s)
-    return s == nil or s == ""
-end
+-- local function empty(s)
+--     return s == nil or s == ""
+-- end
 
 local _token = ngx.var.arg_token
 local _host = ngx.var.arg_host
@@ -15,6 +15,6 @@ end
 local _id = set_var.set_encrypt_session(_token)
 local _session = set_var.set_encode_base32(_id)
 ngx.header.location = "https://" .. _host .. "/" .. _token .. "/?session=" .. _session
-ngx.exit(307)
+ngx.exit(308)
 
 return
