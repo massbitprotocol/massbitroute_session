89b70bc8e212909398a8a6e75b6cbc4356cc49ee
Vu Tran <baysao@gmail.com>
2022-03-30 15:50:55 +0700

    Refactor multi env

--- config.lua
+++ config.lua
@@ -9,12 +9,11 @@ local _config = {
     templates = {},
     apps = {},
     supervisor = [[
-[program:monitor_client]
+-[program:monitor_client]
 command=/bin/bash _SITE_ROOT_/etc/mkagent/agents/push.sh _SITE_ROOT_
 autorestart=true
 redirect_stderr=true
 stdout_logfile=_SITE_ROOT_/logs/monitor_client.log
-
     ]]
 }
 return _config
