3403f41f329c0cf1291801562a19a94a1f6d43a2
Vu Tran <baysao@gmail.com>
2022-08-30 15:06:04 +0700

    refactor: return url with scheme as request

--- src/filter-jsonrpc-access.lua
+++ src/filter-jsonrpc-access.lua
@@ -8,13 +8,14 @@ local set_var = ndk.set_var
 
 local _token = ngx.var.arg_token
 local _host = ngx.var.arg_host
+local _scheme = ngx.var.scheme
 if not _token or not _host then
     ngx.exit(ngx.HTTP_BAD_REQUEST)
 end
 
 local _id = set_var.set_encrypt_session(_token)
 local _session = set_var.set_encode_base32(_id)
-ngx.header.location = "https://" .. _host .. "/" .. _token .. "/?session=" .. _session
+ngx.header.location = _scheme .. "://" .. _host .. "/" .. _token .. "/?session=" .. _session
 ngx.exit(308)
 
 return
